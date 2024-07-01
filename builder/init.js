006e5eaca7835c6cfe3c0fac750ee0fed7127c9e
TriplehashRanchi <168893404+TriplehashRanchi@users.noreply.github.com>
2024-07-01 12:57:47 +0530

    Update init.js

--- builder/init.js
+++ builder/init.js
@@ -1,3 +1,68 @@
+// const axios = require("axios").default;
+// const path = require("path");
+// const fs = require("fs");
+// const setPic = require("./getPic");
+// const genIndex = require("./genIndex");
+// const {
+//   generateMarkupLocal,
+//   generateMarkupRemote,
+// } = require("./generateMarkup");
+
+// require("dotenv").config();
+
+// if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
+// if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
+
+// const picPath = process.env.PIC;
+// const msgPath = process.env.SCROLL_MSG;
+
+// //Local initialization
+// const setLocalData = async () => {
+//   try {
+//     const pic = path.join(__dirname, "../local/", picPath);
+//     let markup = "";
+//     if (msgPath) {
+//       const text = fs.readFileSync(path.join(__dirname, "../local/", msgPath), {
+//         encoding: "utf-8",
+//       });
+//       markup = generateMarkupLocal(text);
+//     }
+//     await setPic(pic);
+//     genIndex(markup);
+//   } catch (e) {
+//     throw new Error(e.message);
+//   }
+// };
+
+// //Remote initialization
+// const setRemoteData = async () => {
+//   try {
+//     let res = await axios.get(picPath, {
+//       responseType: "arraybuffer",
+//     });
+//     const pic = res.data;
+//     let markup = "";
+//     if (msgPath) {
+//       const article = msgPath.split("/").pop();
+//       res = await axios.get(
+//         `https://api.telegra.ph/getPage/${article}?return_content=true`
+//       );
+//       const { content } = res.data.result;
+//       markup = content.reduce(
+//         (string, node) => string + generateMarkupRemote(node),
+//         ""
+//       );
+//     }
+//     await setPic(pic);
+//     genIndex(markup);
+//   } catch (e) {
+//     throw new Error(e.message);
+//   }
+// };
+
+// if (process.argv[2] === "--local") setLocalData();
+// else if (process.argv[2] === "--remote") setRemoteData();
+// else console.log("Fetch mode not specified.");
 const axios = require("axios").default;
 const path = require("path");
 const fs = require("fs");
@@ -16,7 +81,7 @@ if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
 const picPath = process.env.PIC;
 const msgPath = process.env.SCROLL_MSG;
 
-//Local initialization
+// Local initialization
 const setLocalData = async () => {
   try {
     const pic = path.join(__dirname, "../local/", picPath);
@@ -34,7 +99,7 @@ const setLocalData = async () => {
   }
 };
 
-//Remote initialization
+// Remote initialization with GitHub fallback
 const setRemoteData = async () => {
   try {
     let res = await axios.get(picPath, {
@@ -44,14 +109,25 @@ const setRemoteData = async () => {
     let markup = "";
     if (msgPath) {
       const article = msgPath.split("/").pop();
-      res = await axios.get(
-        `https://api.telegra.ph/getPage/${article}?return_content=true`
-      );
-      const { content } = res.data.result;
-      markup = content.reduce(
-        (string, node) => string + generateMarkupRemote(node),
-        ""
-      );
+      try {
+        res = await axios.get(`https://api.telegra.ph/getPage/${article}?return_content=true`);
+        const { content } = res.data.result;
+        markup = content.reduce(
+          (string, node) => string + generateMarkupRemote(node),
+          ""
+        );
+      } catch (e) {
+        // Telegraph is down, fallback to GitHub
+        console.error("Telegraph is down, using GitHub backup");
+        const repoOwner = "TriplehashRanchi";  // Replace with your GitHub username
+        const repoName = "projject";  // Replace with your repository name
+        const filePath = msgPath;  // Path to the file in the repository
+
+        const url = `https://raw.githubusercontent.com/${repoOwner}/${repoName}/main/${filePath}`;
+        res = await axios.get(url);
+        const text = res.data;
+        markup = generateMarkupLocal(text);
+      }
     }
     await setPic(pic);
     genIndex(markup);
@@ -63,3 +139,4 @@ const setRemoteData = async () => {
 if (process.argv[2] === "--local") setLocalData();
 else if (process.argv[2] === "--remote") setRemoteData();
 else console.log("Fetch mode not specified.");
+
